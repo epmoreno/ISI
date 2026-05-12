5a13a7b347cd7ed8afdfb63115cdc729aab36b1a
eantonioaa <e.antonioaa@go.ugr.es>
2026-05-12 18:54:25 +0200

    Login completo con rutas protegidas y cerrar sesion

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,7 +1,21 @@
+import { auth } from '../firebase'
+import { signOut } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
+
 function Navbar() {
+  const navigate = useNavigate()
+
+  const cerrarSesion = async () => {
+    await signOut(auth)
+    navigate('/')
+  }
+
   return (
-    <nav className="navbar navbar-dark bg-dark px-4 w-100">
+    <nav className="navbar navbar-dark bg-dark px-4 d-flex justify-content-between w-100">
       <span className="navbar-brand fw-bold fs-4">OmniLink</span>
+      <button className="btn btn-outline-light btn-sm" onClick={cerrarSesion}>
+        Cerrar sesión
+      </button> 
     </nav>
   )
 }
